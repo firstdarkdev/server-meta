af8751cae0b792b7954e74177a9001f131ab4231
hypherionmc <hypherionmc@gmail.com>
2024-08-09 16:33:53 +0200

    [FEAT] Add routes to get individual versions

--- src/main/java/dev/firstdark/servermeta/server/MetaWebServer.java
+++ src/main/java/dev/firstdark/servermeta/server/MetaWebServer.java
@@ -41,16 +41,30 @@ public class MetaWebServer {
     protected MetaWebServer() {
         javalin = Javalin.create(c -> c.plugins.enableCors(cors -> cors.add(CorsPluginConfig::anyHost)));
         javalin.before(ctx -> ctx.header(Header.ACCESS_CONTROL_ALLOW_ORIGIN, "*").header(Header.ACCESS_CONTROL_ALLOW_HEADERS, "Origin, X-Requested-With, Content-Type, Accept"));
-        javalin.routes(() -> path("v1/{platform}", () -> get("/", this::getAllVersions)));
+        javalin.routes(() -> path("v1/{platform}", () -> {
+            get("/", this::getAllVersions);
+            get("/{version}", this::getVersion);
+        }));
+    }
+
+    private void getVersion(Context context) {
+        String platform = context.pathParam("platform");
+        Class<?> t = routes.get(platform);
+
+        try {
+            IVersionTable result = (IVersionTable) DatabaseController.INSTANCE.getDB().findById(context.pathParam("version"), t);
+            context.json(result.toVersionResponse());
+            return;
+        } catch (Exception ignored) {}
+
+        context.status(404);
     }
 
     private void getAllVersions(Context context) {
         String platform = context.pathParam("platform");
         Class<?> t = routes.get(platform);
 
-        List<VersionResponse> responses = new ArrayList<>();
-
-        DatabaseController.INSTANCE.getDB().getCollection(t).forEach(tt -> responses.add(((IVersionTable) tt).toVersionResponse()));
+        List<VersionResponse> responses = new ArrayList<>(DatabaseController.INSTANCE.getDB().getCollection(t).stream().map(tt -> ((IVersionTable) tt).toVersionResponse()).toList());
 
         // Note here: Minecraft, Fabric and Quilt versions CAN be unordered in the Database.
         // We sort them here to make sure they are in the correct order
